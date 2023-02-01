bee333fef1cb49ed6a9ad76f350ce93b88ddbef7
Lethe <37437842+0xlethe@users.noreply.github.com>
2023-02-01 22:25:09 +0900

    feat: reanimated v2 sample

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { Button } from "react-native";
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from "react-native-reanimated";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Text } from "~/component/atoms/Text";
 import axiosMockInstance, { axiosMockAdapterInstance } from "~/network/mock";
 import { useThemeStyle } from "~/styles/theme";
 
@@ -26,6 +27,14 @@ export const Home: React.FC = (props) => {
 			.catch(setMockInfo);
 	}, []);
 
+	const value = useSharedValue(0);
+	const animatedStyle = useAnimatedStyle(() => ({
+		width: value.value * 350
+	}));
+
+	const toggle = () => {
+		value.value = withSpring(Math.random());
+	};
 	return (
 		<SafeAreaView
 			style={{
@@ -36,7 +45,9 @@ export const Home: React.FC = (props) => {
 				backgroundColor: theme.color.white
 			}}
 		>
-			{mockInfo && <Text style={{ ...theme.fontFamily.Bold }}>{JSON.stringify(mockInfo, null, "  ")}</Text>}
+			<Animated.View style={[{ height: 80, backgroundColor: "black", margin: 30 }, animatedStyle]} />
+			<Button title='toggle' onPress={toggle} />
+			{mockInfo && <Animated.Text style={{ ...theme.fontFamily.Bold }}>{JSON.stringify(mockInfo, null, "  ")}</Animated.Text>}
 		</SafeAreaView>
 	);
 };
