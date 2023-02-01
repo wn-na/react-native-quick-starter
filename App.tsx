9bfba4b438bea7a5a6065e9fca1b00fbb15f9105
Lethe <37437842+0xlethe@users.noreply.github.com>
2023-02-01 11:52:30 +0900

    test: simple axios test

--- App.tsx
+++ App.tsx
@@ -8,123 +8,113 @@
  * @format
  */
 
-import React, {type PropsWithChildren} from 'react';
-import CodePush from 'react-native-code-push';
-import {
-  Platform,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, { useEffect, type PropsWithChildren } from "react";
+import { Platform, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from "react-native";
+import CodePush from "react-native-code-push";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import Config from './Config';
+import { Colors, DebugInstructions, Header, LearnMoreLinks, ReloadInstructions } from "react-native/Libraries/NewAppScreen";
+import { axiosInstance } from "~/network/axios";
+import Config from "./Config";
 
 const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+	PropsWithChildren<{
+		title: string;
+	}>
+> = ({ children, title }) => {
+	const isDarkMode = useColorScheme() === "dark";
+	useEffect(() => {
+		axiosInstance
+			.get("api/health_check")
+			.then((res) => {
+				console.table(res.data);
+			})
+			.catch(console.warn);
+	}, []);
+
+	return (
+		<View style={styles.sectionContainer}>
+			<Text
+				style={[
+					styles.sectionTitle,
+					{
+						color: isDarkMode ? Colors.white : Colors.black
+					}
+				]}
+			>
+				{title}
+			</Text>
+			<Text
+				style={[
+					styles.sectionDescription,
+					{
+						color: isDarkMode ? Colors.light : Colors.dark
+					}
+				]}
+			>
+				{children}
+			</Text>
+		</View>
+	);
 };
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+	const isDarkMode = useColorScheme() === "dark";
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+	const backgroundStyle = {
+		backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
+	};
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView style={backgroundStyle}>
+			<StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} backgroundColor={backgroundStyle.backgroundColor} />
+			<ScrollView contentInsetAdjustmentBehavior='automatic' style={backgroundStyle}>
+				<Header />
+				<View
+					style={{
+						backgroundColor: isDarkMode ? Colors.black : Colors.white
+					}}
+				>
+					<Section title='Step One'>
+						Edit <Text style={styles.highlight}>App.tsx</Text> to change this screen and then come back to see your edits.
+					</Section>
+					<Section title='See Your Changes'>
+						<ReloadInstructions />
+					</Section>
+					<Section title='Debug'>
+						<DebugInstructions />
+					</Section>
+					<Section title='Learn More'>Read the docs to discover what to do next:</Section>
+					<LearnMoreLinks />
+				</View>
+			</ScrollView>
+		</SafeAreaView>
+	);
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+	sectionContainer: {
+		marginTop: 32,
+		paddingHorizontal: 24
+	},
+	sectionTitle: {
+		fontSize: 24,
+		fontWeight: "600"
+	},
+	sectionDescription: {
+		marginTop: 8,
+		fontSize: 18,
+		fontWeight: "400"
+	},
+	highlight: {
+		fontWeight: "700"
+	}
 });
 const codePushOptions = {
-  checkFrequency: Platform.select({
-    ios: CodePush.CheckFrequency.ON_APP_RESUME,
-    android: CodePush.CheckFrequency.ON_APP_START,
-  }),
-  installMode: CodePush.InstallMode.IMMEDIATE,
+	checkFrequency: Platform.select({
+		ios: CodePush.CheckFrequency.ON_APP_RESUME,
+		android: CodePush.CheckFrequency.ON_APP_START
+	}),
+	installMode: CodePush.InstallMode.IMMEDIATE
 };
 
 export default Config.codepush ? CodePush(codePushOptions)(App) : App;
