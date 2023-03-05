9bf7e0547fde2430c0b2ef98c5db19d3fca5a4c4
Lethe <37437842+0xlethe@users.noreply.github.com>
2023-03-05 15:53:50 +0900

    feat: source cleanup

--- App.tsx
+++ App.tsx
@@ -1,71 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { Appearance, Platform } from "react-native";
+import { Route } from "@navigation/route";
+import React from "react";
+import { Platform } from "react-native";
 import CodePush from "react-native-code-push";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-
-import { Fcm } from "@utils/push/firebaseCloudMessage";
-import { LocalNotification } from "@utils/push/localNotification";
 import Config from "react-native-config";
-import SplashScreen from "react-native-splash-screen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
-import { ThemeProvider } from "styled-components";
-import { MainStackNavigation } from "~/navigation/mainStack";
-import theme from "~/styles/theme";
 
 const App = () => {
-	const [appTheme, setAppTheme] = useState<keyof typeof theme>("light");
-
 	/**
 	 * if you want debug use this
 	 * https://github.com/bgaleotti/react-query-native-devtools/tree/main/packages/react-query-native-devtools
 	 */
 	const queryClient = new QueryClient();
-
-	useEffect(() => {
-		Appearance.addChangeListener(({ colorScheme }) => {
-			setAppTheme(Appearance.getColorScheme() === "dark" ? "dark" : "light");
-		});
-		setTimeout(() => {
-			SplashScreen.hide();
-		}, 2000);
-	}, []);
-
-	useEffect(() => {
-		if (Config.USE_PUSH == "true") {
-			if (Config.USE_FIREBASE == "true") {
-				Fcm.register(onRegister, onNotification, onOpenNotification);
-			}
-			LocalNotification.configure(console.log);
-		}
-	}, []);
-
-	const onRegister = (token: string) => {
-		console.log("onRegister:", token);
-	};
-
-	const onNotification = (notification: any) => {
-		console.log("onNotification:", notification);
-		const options = {
-			soundName: "default",
-			playSound: true
-		};
-
-		LocalNotification.sendLocalNotification({ id: 0, data: notification, options });
-	};
-
-	const onOpenNotification = (notification: any, type: "QUIT" | "RUNNING") => {
-		console.log("onOpenNotification:", type, notification);
-	};
-
 	return (
 		<RecoilRoot>
 			<QueryClientProvider client={queryClient}>
-				<ThemeProvider theme={theme[appTheme]}>
-					<SafeAreaProvider>
-						<MainStackNavigation></MainStackNavigation>
-					</SafeAreaProvider>
-				</ThemeProvider>
+				<SafeAreaProvider>
+					<Route />
+				</SafeAreaProvider>
 			</QueryClientProvider>
 		</RecoilRoot>
 	);
--- src/component/atoms/Text.tsx
+++ src/component/atoms/Text.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { Platform, Text as RNText, TextProps } from "react-native";
-import { AppColors } from "~/styles/colors";
-import { font, FONT_LINE_HEIGHT, FONT_SIZE, FONT_WEIGHT, setFontStyle } from "~/styles/font";
+import { font, FontStyle, setFontStyle } from "~/styles/font";
 import { useThemeStyle } from "~/styles/theme";
 
-export const Text: React.FC<
-	TextProps & {
-		size?: FONT_SIZE;
-		lineHeight?: FONT_LINE_HEIGHT;
-		weight?: FONT_WEIGHT;
-		color?: AppColors;
-	}
-> = ({ style, size = "L", weight = "Regular", lineHeight = "Deafult", color, ...props }) => {
+export const Text: React.FC<TextProps & FontStyle> = ({
+	style,
+	size = "L",
+	weight = "Regular",
+	lineHeight = "Deafult",
+	color,
+	...props
+}) => {
 	const theme = useThemeStyle();
 	return (
 		<RNText
